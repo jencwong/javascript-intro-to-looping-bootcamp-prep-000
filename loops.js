3f97116bf974c13d65c65a27b4db00a6e2dfec46
Jen W <jsc525@gmail.com>
2019-05-27 03:49:05 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -27,15 +27,7 @@ function doWhileLoop(num) {
     }
      do {
       ["I run once regardless"];
-    } while(incrementVariable() < num);
+    } while(incrementVariable() < num) || (incrementVariable() = 0);
   }
   
-  var result = "";
-var i = 0;
-
-do {
-  i = i + 1;
-  result = result + i;
-} while (i < 5);
-
-console.log(result);
+  
