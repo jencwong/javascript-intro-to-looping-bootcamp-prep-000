146263a4118e4684aeb97c8c00156ed0a0352313
Jen W <jsc525@gmail.com>
2019-05-27 03:29:44 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -13,7 +13,9 @@ function whileLoop(n) {
   let countdown = n;
 
     while (countdown > 0) {
-      console.log(--countdown)
+      console.log(--countdown);
     }
     return "done";
   }
+  
+  
