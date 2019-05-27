c0cd99d9d237c22f60894027ee648fd4fca02a8e
Jen W <jsc525@gmail.com>
2019-05-27 03:54:08 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -19,13 +19,6 @@ function whileLoop(n) {
   }
   
 
-function doWhileLoop(array) {
-  function maybeTrue() {
-    return Math.random() >= 0.5
-  }
-
-  do {
-    array.pop();
-  } while (array.length > -1 && maybeTrue());
-    return array;
+function doWhileLoop(num) {
+  
 }
